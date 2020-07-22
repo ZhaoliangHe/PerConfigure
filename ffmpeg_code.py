785f95d9a2bbdbc0e99dbc462610cf9bfb6c20b7
ZhaoliangHe <1024299329@qq.com>
2020-07-22 16:25:02 +0800

    ffmpeg_code.py

--- ffmpeg_code.py
+++ ffmpeg_code.py
@@ -1,9 +1,27 @@
 import os
+import argparse
 from subprocess import call
-path = "./dataset/youtube"
 
-ffmpeg_path = "/home/hezhaoliang/ffmpeg/ffmpeg-4.2.3/ffmpeg"
-image_path = os.path.join(path,"11image_720p/image-%3d.png")
-resolution = "1280x720"
-os.system('mkdir ./dataset/youtube/11image_720p')
-call([ffmpeg_path,"-i",os.path.join(path,"11SecondVideo.mp4"),"-r","15","-s",resolution,"-f","image2",image_path])
+# you can use
+# python ffmpeg_code.py -h #查看可传入参数
+# python ffmpeg_code.py -r 15 -s 1280x720
+
+ffmpeg_path = "/home/hezhaoliang/ffmpeg/ffmpeg-4.2.3/ffmpeg" # instead of yours
+
+def videotoimages(fps,resolution,ffmpeg_path):
+    path = "./dataset/youtube/"
+    images_file = '11images' + '_' + fps + '_' + resolution
+    image_path = path + images_file + '/image-%4d.png'
+    call(["mkdir", path + images_file])
+    call([ffmpeg_path, "-i", os.path.join(path, "11SecondVideo.mp4"), "-r", fps, "-s", resolution, "-f", "image2",
+          image_path])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ffmpeg')
+    parser.add_argument("-r", help="fps", default="15", type=str)
+    parser.add_argument("-s", help="resolution", default="1280x720", type=str)
+    args = parser.parse_args()
+    fps = args.r
+    resolution = args.s
+    videotoimages(fps,resolution,ffmpeg_path)
+
